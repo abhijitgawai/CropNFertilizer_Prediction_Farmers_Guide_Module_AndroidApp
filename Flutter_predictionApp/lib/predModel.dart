54ca825e7971af68a83c57ffd78e99922655e498
abhijitgawai <abhijit.gawai18@vit.edu>
2021-10-13 14:52:39 +0530

    Adding text controller for N

--- Flutter_predictionApp/lib/predModel.dart
+++ Flutter_predictionApp/lib/predModel.dart
@@ -16,12 +16,18 @@ class _PredModelState extends State<PredModel> {
     super.initState();
     predValue = "click predict button";
   }
+  final N_Controller = TextEditingController();
 
-  Future<void> predData() async {
+  Future<void> predData(  ) async { //String strr
     final interpreter = await Interpreter.fromAsset('ml_model.tflite');
     var input = [
       [90.0,42.0,43.0,20.0,82.0,6.0,202.0]
     ];
+
+    //print(strr);
+
+    print('THis is controllers input');
+    print(  N_Controller.text + ' N_Controller.text');
 
     var output = List.filled(22, 0).reshape([1, 22]);
     interpreter.run(input, output);
@@ -46,50 +52,51 @@ class _PredModelState extends State<PredModel> {
 
     this.setState(() {
       //predValue = "abhi";//index.toString();//output[0][0].toString();
+      if (index==0)
+        predValue='Apple';
       if (index==1)
-          predValue='Rice';
+          predValue='Banana';
       if (index==2)
-        predValue='Rice';
+        predValue='Black Gram';
       if (index==3)
-        predValue='Rice';
+        predValue='Chickapea';
       if (index==4)
-        predValue='Rice';
+        predValue='Coconut';
       if (index==5)
-        predValue='Rice';
+        predValue='Coffee';
       if (index==6)
-        predValue='Rice';
+        predValue='Cotton';
       if (index==7)
-        predValue='Rice';
+        predValue='Grapes';
       if (index==8)
-        predValue='Rice';
+        predValue='Jute';
       if (index==9)
-        predValue='Rice';
+        predValue='kidneybeans';
       if (index==10)
-        predValue='Rice';
+        predValue='Lentil';
       if (index==11)
-        predValue='Rice';
-      if (index==12)
-        predValue='Rice';
-      if (index==13)
-        predValue='Rice';
-      if (index==14)
-        predValue='Rice';
-      if (index==15)
-        predValue='Rice';
-      if (index==16)
-        predValue='Rice';
-      if (index==17)
-        predValue='Rice';
-      if (index==18)
-        predValue='Rice';
-      if (index==19)
-        predValue='Rice';
-      if (index==20)
         predValue='Maize';
+      if (index==12)
+        predValue='Mango';
+      if (index==13)
+        predValue='mothbeans';
+      if (index==14)
+        predValue='mungbean';
+      if (index==15)
+        predValue='Muskmelon';
+      if (index==16)
+        predValue='Orange';
+      if (index==17)
+        predValue='Papaya';
+      if (index==18)
+        predValue='pigeonpeas';
+      if (index==19)
+        predValue='Pomegranate';
+      if (index==20)
+        predValue='Rice';
       if (index==21)
-        predValue='Rice';
-      if (index==22)
-        predValue='Rice';
+        predValue='Watermelon';
+
 
 
     });
@@ -105,10 +112,23 @@ class _PredModelState extends State<PredModel> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
+            TextField(
+              controller: N_Controller,
+
+              textAlign: TextAlign.left,
+              decoration: InputDecoration(
+                border: InputBorder.none,
+                hintText: 'PLEASE ENTER YOUR EMAIL',
+                hintStyle: TextStyle(color: Colors.grey),
+              ),
+            ),
+
             Text(
               "change the input values in code to get the prediction",
               style: TextStyle(fontSize: 16),
             ),
+            Text(N_Controller.text),
+
             SizedBox(height: 12),
             MaterialButton(
               color: Colors.blue,
@@ -116,8 +136,13 @@ class _PredModelState extends State<PredModel> {
                 "predict",
                 style: TextStyle(fontSize: 25),
               ),
+
               onPressed: predData,
-            ),
+              //onPressed: () {
+              //  predData( 'abhi' );
+              //},
+
+    ),
             SizedBox(height: 12),
             Text(
               "Predicted value :  $predValue ",
@@ -129,3 +154,4 @@ class _PredModelState extends State<PredModel> {
     );
   }
 }
+
